95030e80c84a1a0a5eba962a41c0ebcf4812fbf1
nemz6406 <namhoang6406@gmail.com>
2026-09-15 22:52:50 +0700

    Slot2: Hoàn thành TODO 3 - Thêm phương thức update và delete vào EmployeeDAO

--- Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200319/dao/EmployeeDAO.java
+++ Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200319/dao/EmployeeDAO.java
@@ -10,6 +10,10 @@ public class EmployeeDAO {
     private static final EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("hsf302FU");
 
+    // ==========================================
+    // TODO 1 & 2: Xây dựng các phương thức cơ bản (Create, Read)
+    // ==========================================
+
     public void save(Employee employee) {
         EntityManager em = emf.createEntityManager();
         try {
@@ -30,6 +34,45 @@ public class EmployeeDAO {
         EntityManager em = emf.createEntityManager();
         try {
             return em.find(Employee.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    // ==========================================
+    // TODO 3: Bổ sung phương thức Update và Delete
+    // ==========================================
+
+    public void update(Employee employee) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(employee);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Employee employee = em.find(Employee.class, id);
+            if (employee != null) {
+                em.remove(employee);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
         } finally {
             em.close();
         }
